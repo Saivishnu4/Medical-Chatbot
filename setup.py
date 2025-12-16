26273d763a25d98420437be8f25a67c18cf69182
Saivishnu4 <roadhash06@gmail.com>
2025-12-16 17:08:22 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Saivishnu',
+    author_email= 'roadhash06@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
